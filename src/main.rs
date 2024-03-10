7f562c58517b1f664513b15c1ae184a789c9eb84
Ameer Ibrahim <ameer@Ameers-MacBook-Pro.local>
2024-03-10 16:07:53 +0530

    Add: reading args from command lines

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::env;
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let file_name = &args[2];
+    println!("searching for string: {query}");
+    println!("in a file: {}", file_name)
 }
