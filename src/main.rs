2fa271060a3b95fd3cb178fa4de4d97450441f38
Ameer Ibrahim <ameer@Ameers-MacBook-Pro.local>
2024-03-10 16:51:06 +0530

    Add: reading a given file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use std::env;
+use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let query = &args[1];
     let file_name = &args[2];
     println!("searching for string: {query}");
-    println!("in a file: {}", file_name)
+    println!("in a file: {}", file_name);
+
+    let contents = fs::read_to_string(file_name).expect("not able to open a file");
+
+    println!("file contents:\n{contents}")
 }
