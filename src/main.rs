168cec5e9e5f95b4d2f4061cdf0470df2db9583a
Ameer Ibrahim <ameer@Ameers-MacBook-Pro.local>
2024-03-11 23:58:24 +0530

    Add: config struct for given config

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,22 @@ use std::env;
 use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_name = &args[2];
-    println!("searching for string: {query}");
-    println!("in a file: {}", file_name);
+    let config = Config::new(&args);
 
-    let contents = fs::read_to_string(file_name).expect("not able to open a file");
+    let contents = fs::read_to_string(config.file_name).expect("not able to open a file");
 
     println!("file contents:\n{contents}")
+}
+
+struct Config {
+    query: String,
+    file_name: String,
+}
+
+impl Config {
+    fn new(args: &Vec<String>) -> Config {
+        let query = args[1].clone();
+        let file_name = args[2].clone();
+        Config { query, file_name }
+    }
 }
