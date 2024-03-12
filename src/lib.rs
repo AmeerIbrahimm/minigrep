cd2bf63a8f30ed39cf13a666940d6304bdf72b58
Ameer Ibrahim <ameer@Ameers-MacBook-Pro.local>
2024-03-12 17:15:28 +0530

    Add: moved application code as module

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+use std::error::Error;
+use std::fs;
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_name)?;
+    println!("file contents:\n{contents}");
+    Ok(())
+}
+
+pub struct Config {
+    pub query: String,
+    pub file_name: String,
+}
+
+impl Config {
+    pub fn build(args: &Vec<String>) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_name = args[2].clone();
+        Ok(Config { query, file_name })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,16 @@
 use std::env;
-use std::fs;
+use std::process;
+
+use minigrep::Config;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
-
-    let contents = fs::read_to_string(config.file_name).expect("not able to open a file");
-
-    println!("file contents:\n{contents}")
-}
-
-struct Config {
-    query: String,
-    file_name: String,
-}
-
-impl Config {
-    fn new(args: &Vec<String>) -> Config {
-        let query = args[1].clone();
-        let file_name = args[2].clone();
-        Config { query, file_name }
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("problem in parsing args: {err}");
+        process::exit(1);
+    });
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
     }
 }
