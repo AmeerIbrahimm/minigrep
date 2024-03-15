ffd8a301a0b95939bd1b9858deecb562eba59fbe
AmeerIbrahimm <ameerthecoder@gmail.com>
2024-03-15 17:15:17 +0530

    Add: changes in error messages

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() {
         process::exit(1);
     });
     if let Err(e) = minigrep::run(config) {
-        eprintln!("Application error: {e}");
+        eprintln!("Application error occured: {e}");
         process::exit(1);
     }
 }
